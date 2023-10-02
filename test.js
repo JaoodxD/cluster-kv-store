c8919ce54ae7d88f124283fff689296c87d54023
JaoodxD <jaood96@gmail.com>
2023-10-02 17:22:04 +0300

    Update tests

--- test.js
+++ test.js
@@ -16,16 +16,51 @@ const ClusteredStorage = require('./lib/storage/clusteredStorage.js');
 const test1 = async () => {
   const storage = ClusteredStorage({
     type: 'object',
-    TTL: 0,
+    TTL: 1000,
     norm: 0,
     factor: 1,
   });
 
   await storage.hset('CRM#1', '1001', { locker: true, user: '__admin__' });
-  const res = await storage.hget('CRM#1', '1001');
-  console.log(res);
+  const res1 = await storage.hget('CRM#1', '1001');
+  console.log(res1);
+  await wait(983);
+  const res2 = await storage.hget('CRM#1', '1001');
+  console.log(res2);
+
   storage.shutdown();
 };
 tests.push(test1);
+
+const test2 = async () => {
+  const storage = ClusteredStorage({
+    type: 'object',
+    TTL: 1000,
+    norm: 0,
+    factor: 1,
+  });
+
+  const promises1 = [];
+  console.time('insertion');
+  for (let i = 0; i < 10000; i++) {
+    const info = { locked: true, user: '__system__' };
+    promises1.push(storage.hset('CRM#1', i, info));
+  }
+  await Promise.all(promises1);
+  console.timeEnd('insertion');
+
+  const promises2 = [];
+  console.time('retrieving');
+  for (let i = 0; i < 10000; i++) {
+    promises2.push(storage.hget('CRM#1', i));
+  }
+  const res = await Promise.all(promises2);
+  console.timeEnd('retrieving');
+  console.log(res);
+
+
+  storage.shutdown();
+};
+tests.push(test2);
 
 chainExec(tests);  
